af7c0b9ec577fb9d0892b4f9ff48b51998ce5361
Muhammad Daud <86343612+MDaud32@users.noreply.github.com>
2022-10-30 10:34:51 +0500

    adding feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import logo from './images/logo.svg';
+import intractive from './images/desktop/image-interactive.jpg';
 function App() {
   return (
     <div className="">
@@ -45,6 +46,21 @@ function App() {
           <div className="md:text-6xl text-3xl text-white p-4 py-10 items-left md:items-center uppercase md:max-w-lg max-w-sm border border-white my-36 ">
             impressive experiences that deliver
           </div>
+        </div>
+      </div>
+      {/* interactive vr */}
+      <div className="flex flex-col md:flex-row max-w-6xl mx-auto my-36 px-6 relative container md:px-0">
+        <img src={intractive} alt="" className="" />
+        <div className="top-48 bg-white md:absolute md:right-0 md:py-20 md:pl-20">
+          <h2 className="text-4xl max-w-lg mb-6 mt-10 text-center text-gray-900 uppercase md:text-5xl md:mt-0 md:text-left">
+            THE LEADER IN INTERACTIVE VR
+          </h2>
+          <p className="text-gray-900 max-w-md">
+            Founded in 2011, Loopstudios has been producing world-class virtual
+            reality projects for some of the best companies around the globe.
+            Our award-winning creations have transformed businesses through
+            digital experiences that bind to their brand.
+          </p>
         </div>
       </div>
     </div>
